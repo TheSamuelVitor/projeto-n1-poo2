2adf618b72dda7e01b2042a227450ee4bd7bff42
TheSamuelVitor <thesamuelvitor.py@gmail.com>
2023-10-05 11:05:43 -0300

    :sparkles: feat: adicionando peca de lente

--- atividade_avaliativa/calcOne/problematicaOptica/src/App.java
+++ atividade_avaliativa/calcOne/problematicaOptica/src/App.java
@@ -5,8 +5,9 @@ public class App {
         PecaOculos peca = new Antirreflexo(oculos);
         peca = new Grau(peca);
         peca = new Transition(peca);
+        peca = new Lente(peca, "Multifocal");
 
-        System.out.println(peca.getNome());
-        System.out.println(peca.getValor());
+        System.out.println("\n" + peca.getNome());
+        System.out.println("Valor: "+ peca.getValor());
     }
 }

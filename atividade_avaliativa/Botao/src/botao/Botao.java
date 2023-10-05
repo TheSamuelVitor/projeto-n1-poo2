43b3f506d1d9737794c0be6605ad2de6e0ccef23
EMANOELGOMS <emanoelsilva.jua@gmail.com>
2023-10-05 00:10:28 -0300

    :sparkles: feat: adicionando resolucao da problematica do botao

---
+++ atividade_avaliativa/Botao/src/botao/Botao.java
@@ -0,0 +1,39 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package botao;
+
+/**
+ *
+ * @author EMANOEL SILVA
+ */
+
+// 3 questao
+public class Botao {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       
+        // Criando uma instância do botão
+        Button botao = new Button();
+
+        // Configurando ações de clique
+        botao.adicionarAcaoDeClique(() -> {
+            System.out.println("Acao 1 executada.");
+        });
+
+        botao.adicionarAcaoDeClique(() -> {
+            System.out.println("Acao 2 executada.");
+        });
+
+        // Simulando o clique do botão
+        botao.clicar();
+     
+        
+        
+        
+    }
+}
---
+++ atividade_avaliativa/Botao/src/botao/Button.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package botao;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author EMANOEL SILVA
+ */
+public class Button {
+    private List<AcaoDoClique> acoesDoClique = new ArrayList<>();
+
+    // Método para adicionar uma ação de clique ao botão
+    public void adicionarAcaoDeClique(AcaoDoClique acao) {
+        acoesDoClique.add(acao);
+    }
+
+    // Método para simular o clique do botão e executar todas as ações configuradas
+    public void clicar() {
+        System.out.println("Botao clicado.");
+        for (AcaoDoClique acao : acoesDoClique) {
+            acao.executar();
+        }
+    }
+    
+}
